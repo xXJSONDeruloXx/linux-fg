74e5dbf0d00c90d537759b24997e8c90282db5a1
xxjsonderuloxx <danielhimebauch@gmail.com>
2025-02-19 10:04:02 -0500

    update readme dependencies

---
+++ CMakeFiles/lossless-scaling.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lossless-scaling.dir/src/frame_manager.cpp.o"
+  "CMakeFiles/lossless-scaling.dir/src/frame_manager.cpp.o.d"
+  "CMakeFiles/lossless-scaling.dir/src/main.cpp.o"
+  "CMakeFiles/lossless-scaling.dir/src/main.cpp.o.d"
+  "CMakeFiles/lossless-scaling.dir/src/scaler.cpp.o"
+  "CMakeFiles/lossless-scaling.dir/src/scaler.cpp.o.d"
+  "CMakeFiles/lossless-scaling.dir/src/vulkan_context.cpp.o"
+  "CMakeFiles/lossless-scaling.dir/src/vulkan_context.cpp.o.d"
+  "CMakeFiles/lossless-scaling.dir/src/window_capture.cpp.o"
+  "CMakeFiles/lossless-scaling.dir/src/window_capture.cpp.o.d"
+  "lossless-scaling"
+  "lossless-scaling.pdb"
+  "shaders/interpolate.comp.spv"
+  "shaders/motion.comp.spv"
+  "shaders/scale.comp.spv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lossless-scaling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/lossless-scaling.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lossless-scaling.
+# This may be replaced when dependencies are built.
